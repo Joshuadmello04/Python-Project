f4945eaa7a2114c6f7867ee38248011aca91c8f2
Joshuadmello04 <joshuadmello777@gmail.com>
2024-04-17 18:58:36 +0530

    some random stuff

--- myenv/generate_keys.py
+++ myenv/generate_keys.py
@@ -4,9 +4,9 @@ from pathlib import Path
 import streamlit_authenticator as stauth
 from streamlit_authenticator.utilities.hasher import Hasher
 
-names = ["Vivian Ludrick", "Joshua Dmello"]
-usernames = ["vivalchemy", "jdmello"]
-passwords = ["87654321", "12345678"] 
+names = ["Vivian Ludrick", "Joshua Dmello","Samuel Dsouza"]
+usernames = ["vivalchemy", "jdmello","spamuel"]
+passwords = ["87654321", "12345678","samspam"] 
 
 hashed_passwords = Hasher(passwords).generate()
 
